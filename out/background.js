589744df51f42aa54c81a341d6fcfd48bebbc4a1
Yash Godara <yash17godara@gmail.com>
2024-09-22 02:19:18 +0530

    background.js added

---
+++ out/background.js
@@ -0,0 +1,2 @@
+// Uninstall Tracking: Sets a URL that users will be directed to upon uninstalling the extension, possibly for feedback or support.
+chrome.runtime.setUninstallURL('https://www.y-g.tech');
